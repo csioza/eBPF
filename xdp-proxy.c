820ac759b30ae0c6672e3079012a167b165e7fcd
dengzhonghua <csioza@163.com>
2022-02-27 15:55:25 +0800

    2

--- xdp-proxy.c
+++ xdp-proxy.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/resource.h>
 #include <bpf/libbpf.h>
 #include <bpf/bpf.h>
 #include <net/if.h>
+#include <arpa/inet.h>
 #include <linux/types.h>
 #include <linux/if_link.h>
 #include "xdp-proxy.skel.h"
+#include "xdp-proxy.h"
 
 /* Attach to eth0 by default */
 #define DEV_NAME "eth0"
@@ -46,6 +49,27 @@ int main(int argc, char **argv)
 	if (err)
 	{
 		fprintf(stderr, "failed to load BPF object %d\n", err);
+		goto cleanup;
+	}
+
+	__be32 svc1_key = SVC1_KEY;
+	struct endpoints ep =
+		{
+			/* fixme: replace with your own values if they are different */
+			.ep1 = inet_addr("172.17.0.2"),						/* Hex:0x20011ac Dec:33558956 */
+			.ep2 = inet_addr("172.17.0.3"),						/* Hex:0x30011ac Dec:50336172 */
+			.client = inet_addr("172.17.0.4"),					/* Hex:0x40011ac Dec:67113388 */
+			.vip = inet_addr("172.17.0.5"),						/* Hex:0x50011ac Dec:83890604 */
+			.ep1_mac = {0x02, 0x42, 0xac, 0x11, 0x00, 0x02},	/* 02:42:ac:11:00:02 */
+			.ep2_mac = {0x02, 0x42, 0xac, 0x11, 0x00, 0x03},	/* 02:42:ac:11:00:03 */
+			.client_mac = {0x02, 0x42, 0xac, 0x11, 0x00, 0x04}, /* 02:42:ac:11:00:04 */
+			.vip_mac = {0x02, 0x42, 0xac, 0x11, 0x00, 0x05},	/* 02:42:ac:11:00:05 */
+		};
+	int map_id = bpf_map__fd(obj->maps.services);
+	err = bpf_map_update_elem(map_id, &svc1_key, &ep, BPF_ANY);
+	if (err)
+	{
+		fprintf(stderr, "failed to update maps\n");
 		goto cleanup;
 	}
 
